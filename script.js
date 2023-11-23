4b46c76a638725fd58e70651af4add41dcb409be
RaizelArief <148044563+RaizelArief@users.noreply.github.com>
2023-11-23 22:50:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// Math Round
+let a = 10.5
+let round = Math.round (a);
+console.log(round);
+
+let b = 15.6
+let angka = Math.round (b);
+console.log(angka);
+
+let c = 20.1
+let nomer = Math.round (c);
+console.log(nomer);
+// Math Floor
+let d = 7.5
+let floor = Math.floor (d);
+console.log(floor);
+
+let e = 12.6
+let bunga = Math.floor (e);
+console.log(bunga);
+
+let f = 20.1
+let mawar = Math.floor (f);
+console.log(mawar);
+// Math Ceil
+let g = 16.5
+let ceil = Math.ceil (g);
+console.log(ceil);
+
+let h = 18.7
+let cc = Math.ceil (h);
+console.log(cc);
+
+let i = 28.1
+let lolita = Math.ceil (i);
+console.log(lolita);
+// Math Pow
+let j = 5
+let k = 3
+let l = 2
+let Pow = Math.pow (5,3)
+console.log(Pow);
+let pangkat = Math.pow (5,2)
+console.log(pangkat);
+let perkalian = Math.pow (2,3)
+console.log(perkalian);
+
+let akar = Math.sqrt (49)
+console.log(akar);
+
+let perkpk = Math.sqrt (25)
+console.log(perkpk);
+
+let pembagian = Math.sqrt (16)
+console.log(pembagian);
